382cf8ba6ccf70ec578f7b60857db3d15875cda9
Tomasz Gospodarczyk <tomasz.gospodarczyk@allegro.pl>
2014-10-05 23:14:36 +0200

    add share buttons

--- js/s.js
+++ js/s.js
@@ -1,16 +1,17 @@
 /*jshint multistr: true */
 /* global reqwest */
-(function () {
-    var renderTemplate = function (data) {
-            var div = document.getElementById('repos-section'),
-                template = new window.t(' \
+(function() {
+    var renderTemplate = function(data, placeholderElement) {
+            var div = placeholderElement,
+                template;
+            template = new window.t(' \
                 <article class="repos-box"><h3>Ralph</h3><ul>{{@ralph}}<li><article class="repo-box"><header><h4><a href="{{=_val.html_url}}">{{=_val.name}}</a></h4><p class="star-gazers"><a href="{{=_val.html_url}}/stargazers"><span>★</span> {{=_val.stargazers_count}}</a></p></header><p class="repo-description">{{=_val.description}}</p></article></li>{{/@ralph}}</ul></article> \
                 <article class="repos-box"><h3>Selena</h3><ul>{{@selena}}<li><article class="repo-box"><header><h4><a href="{{=_val.html_url}}">{{=_val.name}}</a></h4><p class="star-gazers"><a href="{{=_val.html_url}}/stargazers"><span>★</span> {{=_val.stargazers_count}}</a></p></header><p class="repo-description">{{=_val.description}}</p></article></li>{{/@selena}}</ul></article> \
                 <article class="repos-box"><h3>Tools</h3><ul>{{@tools}}<li><article class="repo-box"><header><h4><a href="{{=_val.html_url}}">{{=_val.name}}</a></h4><p class="star-gazers"><a href="{{=_val.html_url}}/stargazers"><span>★</span> {{=_val.stargazers_count}}</a></p></header><p class="repo-description">{{=_val.description}}</p></article></li>{{/@tools}}</ul></article> \
                 <article class="repos-box"><h3>For fun</h3><ul>{{@for_fun}}<li><article class="repo-box"><header><h4><a href="{{=_val.html_url}}">{{=_val.name}}</a></h4><p class="star-gazers"><a href="{{=_val.html_url}}/stargazers"><span>★</span> {{=_val.stargazers_count}}</a></p></header><p class="repo-description">{{=_val.description}}</p></article></li>{{/@for_fun}}</ul></article>');
             div.innerHTML = template.render(data);
         },
-        getRepoSchema = function () {
+        getRepoSchema = function() {
             return {
                 ralph: ['ralph', 'ralph_assets', 'ralph_beast'],
                 selena: ['selena', 'selena-agent'],
@@ -18,12 +19,12 @@
                 for_fun: ['confitura-jamjars', 'jPiknik-dagger', 'devops-challenge']
             };
         },
-        showRepo = function (repositories) {
+        showRepo = function(repositories) {
             var repoFiltered = {},
                 repoStructure = getRepoSchema();
-            repositories.sort(function (a, b) {
+            repositories.sort(function(a, b) {
                 return b.stargazers_count - a.stargazers_count;
-            }).forEach(function (element) {
+            }).forEach(function(element) {
                 for (var repo in repoStructure) {
                     if (repoStructure[repo].indexOf(element.name) !== -1) {
                         if (!repoFiltered[repo]) {
@@ -34,16 +35,31 @@
                 }
             });
             return repoFiltered;
-        };
-    reqwest({
-        // url: 'https://api.github.com/users/allegro/repos?per_page=9000',
-        url: 'js/repositories.json',
-        type: 'json',
-        method: 'get',
-        error: function (err) {
         },
-        success: function (data) {
-            renderTemplate(showRepo(data));
-        }
-    });
+        getRepositories = function(placeholderId) {
+            var placeholderElement = document.getElementById(placeholderId);
+            if (placeholderElement) {
+                reqwest({
+                    // url: 'https://api.github.com/users/allegro/repos?per_page=9000',
+                    url: 'js/repositories.json',
+                    type: 'json',
+                    method: 'get',
+                    error: function(err) {},
+                    success: function(data) {
+                        renderTemplate(showRepo(data), placeholderElement);
+                    }
+                });
+            }
+        },
+        initShareWidget = function() {
+            // share
+            [].slice.call(document.querySelectorAll('.social a')).forEach(function(item) {
+                item.addEventListener('click', function(e) {
+                    window.open(this.href, 'share', 'width=500,height=296');
+                    e.preventDefault();
+                }, false);
+            });
+        };
+    getRepositories('repos-section');
+    initShareWidget();
 })();
