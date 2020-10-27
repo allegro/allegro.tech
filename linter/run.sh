5123bd9e0e326f7c840d8bf755b73db9d1903e8b
Waldemar Panas <waldemar.panas@allegro.pl>
2020-10-27 15:01:15 +0100

    Fix linting

--- linter/run.sh
+++ linter/run.sh
@@ -1,8 +1,17 @@
 path=$(dirname "$0")
-errors=$(git diff --name-only --diff-filter=AMC master..HEAD | grep _posts | sed "s~^~$path/../~" | sed "s~$~ -c $path/.markdownlint.json~" | xargs npx markdownlint-cli 2>&1 | sed "s~^./../~~")
+files_to_check=$(git diff --name-only --diff-filter=AMC master..HEAD | grep _posts | sed "s~^~$path/../~" | sed "s~$~ -c $path/.markdownlint.json~")
+
+if test -z "$files_to_check"
+then
+    echo 1>&2 "No files to check. Remember to commit changes."
+    exit 0
+else
+    errors=$(echo "$files_to_check" | xargs npx markdownlint-cli 2>&1 | sed "s~^./../~~" | tail -n +2)
+fi
 
 if test -z "$errors" 
 then
+    echo 1>&2 "No issues found."
     exit 0
 else
     echo 1>&2 "$errors"
