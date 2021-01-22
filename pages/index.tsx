90676ae97c3d683a54bb0d3131ac50f1bdb70282
Bartosz Galek <bartosz.galek@allegro.pl>
2021-01-22 11:18:04 +0100

    google analytics code updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,14 +48,13 @@ const HomePage: React.FunctionComponent<HomePageProps> = ({ posts, jobs, events,
                 <link rel="shortcut icon" href="favicon.ico"/>
                 <link rel="canonical" href="https://allegro.tech" itemProp="url"/>
                 <link rel="preload" href="images/splash.jpg" as="image" />
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-1M1FJ5PXWW"></script>
                 <script dangerouslySetInnerHTML={{
                     __html: `
-                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                    ga('create', 'UA-55852483-1', 'auto');
-                    ga('send', 'pageview');
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-1M1FJ5PXWW');
                 `
                 }}>
                 </script>
