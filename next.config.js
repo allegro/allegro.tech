f864554f09610fb02e301b9f1ce580d94663080b
Tomasz Gospodarczyk <tomasz.gospodarczyk@allegro.pl>
2021-01-22 09:21:04 +0100

    add redirect to podcast

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const permalinks = require('./permalinks.json');
 
 module.exports = {
     basePath: process.env.BASE_PATH,
+    trailingSlash: true,
     async exportPathMap(defaultPathMap) {
-        return { ...defaultPathMap, ...permalinks };
+        return {...defaultPathMap, ...permalinks };
     }
 }
