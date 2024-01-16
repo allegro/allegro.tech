7044db21c089b501795003abb10f88eb22af7c85
Bartosz Galek <bartosz.galek@allegro.com>
2024-01-16 20:37:02 +0100

    removed fb pixel

--- components/Tracking.tsx
+++ components/Tracking.tsx
@@ -3,8 +3,6 @@ import React from "react";
 const Tracking: React.FunctionComponent = () => {
     return (
         <div style={{ visibility: 'hidden', height: 0, overflow: 'hidden', position: 'relative'}}>
-            <img alt="fb" height="1" width="1" style={{ position: 'absolute'}}
-                 src="https://www.facebook.com/tr?id=1650870088530325&ev=PageView&noscript=1"/>
         </div>
     )
 };
