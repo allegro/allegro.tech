1795cae24b4db172695337be4ef3032b729ee704
bgalek <bgalek@users.noreply.github.com>
2024-02-22 10:04:11 +0000

    deploy: 1f924780184b393992c98adc0c53c1a1f523afd3

---
+++ clear-cookies.js
@@ -0,0 +1,2 @@
+// just to clean up old google cookies, remove this in few months
+document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
